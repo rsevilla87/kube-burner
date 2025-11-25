22301ed1844d597dbc8f51a9b816484fe664c40b
Raul Sevilla <rsevilla@redhat.com>
2025-11-25 13:47:08 +0100

    update prom
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -42,7 +42,7 @@ func NewPrometheusClient(configSpec config.Spec, url string, auth Auth, step tim
 		indexer:    indexer,
 		metadata:   metadata,
 	}
-	log.Infof("👽 Initializing prometh   eus  client with URL: %s", url)
+	log.Infof("👽 Initializing pro meth   eus  client with URL: %s", url)
 	p.Client, err = prometheus.NewClient(url, auth.Token, auth.Username, auth.Password, auth.SkipTLSVerify)
 	return &p, err
 }
